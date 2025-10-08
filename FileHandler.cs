e6aa144527000f65c177f1244159d4b343b404a1
Hanna <206832485+klisterfisken@users.noreply.github.com>
2025-10-08 13:51:47 +0200

    Move list declaration within ReadFromFile

--- FileHandler.cs
+++ FileHandler.cs
@@ -5,40 +5,40 @@ namespace AddressBookProject
     public class FileHandler
     {
 
+
         //ToText - Gör om data så det kan skrivas på en rad i txt-filen
-        public static string ToText(Contact contact)
+        public string ToText(Contact contact)
         {
             return $"{contact.Name},{contact.StreetAddress},{contact.PostalCode},{contact.City},{contact.Phone},{contact.Email}";
         }
 
-        public static void WriteToFile(Contact contact)
+        public void WriteToFile(Contact contact)
         {
             using (StreamWriter writer = new StreamWriter("contacts.txt", append: true))
             {
 
-                writer.WriteLine(FileHandler.ToText(contact));
+                writer.WriteLine(ToText(contact));
             }
         }
 
         //ReadFromFile - hämtar ut en lista med kontakter
         public List<Contact> ReadFromFile()
         {
+            List<Contact> contactsList = new List<Contact>(); // Deklarera och instansiera lista
+
             using (StreamReader reader = new StreamReader("contacts.txt"))
             {
-                contactsList = new List<Contact>();//instansiera listan
-
                 string? line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    contactsList.Add(FromText(line));//lägger till varje kontakt i listan
+                    contactsList.Add(FromText(line));
                 }
             }
-
             return contactsList;
         }
 
         //FromText - Läser ut objektet från en rad i txt-filen
-        public static Contact FromText(string line)
+        public Contact FromText(string line)
         {
             string[] parts = line.Split(',');
             return new Contact
